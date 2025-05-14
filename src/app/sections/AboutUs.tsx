c2dd1f42923f18f2b4a0af4acc80710561048aa5
RodrI <rodri@supracode.eu>
2025-05-15 00:43:52 +0200

    improve image section about us

--- src/app/sections/AboutUs.tsx
+++ src/app/sections/AboutUs.tsx
@@ -13,19 +13,14 @@ export const AboutUs = () => {
         width={1000}
         className="!h-full w-10/12 rounded shadow-2xl about-us-image md:block hidden"
       />
-      <Image
-        src="/avba-refugee.jpeg"
-        alt="refugio"
-        height={1000}
-        width={1000}
-        className="!h-full shadow-2xl block bg-cover bg-no-repeat md:hidden"
-      />
-      <div className="absolute md:w-96 md:left-2 px-2 md:px-0 w-full top-0 pt-24 md:top-auto">
-        <div className="md:bg-white backdrop-blur-xs md:backdrop-blur-none rounded-lg shadow-xl p-8 flex flex-col items-start justify-center">
-          <h1 className="text-3xl font-bold text-amber-100  md:text-amber-300 mb-4">
+      <div className="bg-[url('/avba-refugee.jpeg')] bg-cover bg-no-repeat w-full h-full absolute top-0 left-0 md:hidden" />
+
+      <div className="absolute md:w-96 md:left-2 px-2 md:px-0 w-full md:top-auto">
+        <div className="md:bg-white backdrop-blur-xs backdrop-brightness-50 md:backdrop-brightness-100 text-justify md:backdrop-blur-none rounded-lg shadow-2xl p-8 flex flex-col items-start justify-center">
+          <h1 className="text-3xl font-bold text-amber-200  md:text-amber-300 mb-4">
             ¿QUIÉNES SOMOS?
           </h1>
-          <p className="md:text-gray-700 text-white text-lg mb-4">
+          <p className="md:text-gray-700 text-amber-100 text-lg mb-4">
             La Asociación de Voluntarios por el Bienestar Animal de Láchar y
             Peñuelas (AVBA) es una entidad sin ánimo de lucro fundada en 2021.
             Su objetivo es ayudar a los animales abandonados en los pueblos de
