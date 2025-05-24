15f47085f194e49749b44b6489ed5dc9b091d0da
RodrI <rodri@supracode.eu>
2025-05-24 23:16:40 +0200

    add insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Header } from './components/Header';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -33,6 +34,7 @@ export default function RootLayout({
         <div className="absolute top-0 left-0 w-screen h-screen bg-white z-0">
           {children}
         </div>
+        <SpeedInsights />
       </body>
     </html>
   );
